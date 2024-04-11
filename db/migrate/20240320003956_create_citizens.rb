583721b9dd9ff760842c5c6ac4134fc8bf22242b
Gabriel Barboza <gabrielmbarboza@gmail.com>
2024-04-11 00:58:44 -0300

    Adds ActiveStorage support. Updates the citizen and address migrations

--- db/migrate/20240320003956_create_citizens.rb
+++ db/migrate/20240320003956_create_citizens.rb
@@ -1,13 +1,13 @@
 class CreateCitizens < ActiveRecord::Migration[7.1]
   def change
     create_table :citizens do |t|
-      t.string :full_name
-      t.string :cpf
-      t.string :cns
-      t.string :email
-      t.date :date_of_birth
-      t.string :phone
-      t.integer :status
+      t.string :full_name, null: false
+      t.string :cpf, null: false, index: { unique: true, name: 'unique_cpf' }
+      t.string :cns, null: false, index: { unique: true, name: 'unique_cns' }
+      t.string :email, null: false, index: { unique: true, name: 'unique_emails' }
+      t.date :date_of_birth, null: false
+      t.string :phone, null: false
+      t.integer :status, null: false
 
       t.timestamps
     end
